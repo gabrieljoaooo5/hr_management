2e429d124d45a2805427d84570ca0d07a6c11163
Joao Gabriel <joaogabriel1598@gmail.com>
2021-12-25 15:15:18 -0300

    Creating employee CRUD

--- apps/employees/urls.py
+++ apps/employees/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import home
+from .views import (
+    EmployeeList,
+    EmployeeEdit,
+    EmployeeDelete,
+    EmployeeCreate
+)
 
 urlpatterns = [
-    path('', home),
+    path('', EmployeeList.as_view(), name='list_employees'),
+    path('new/', EmployeeCreate.as_view(), name='create_employee'),
+    path('edit/<int:pk>/', EmployeeEdit.as_view(), name='update_employee'),
+    path('delete/<int:pk>/', EmployeeDelete.as_view(), name='delete_employee'),
+
 ]
--- apps/employees/views.py
+++ apps/employees/views.py
@@ -1,6 +1,40 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    UpdateView,
+    DeleteView,
+    CreateView
+)
+from .models import Employee
 
 
-def home(request):
-    return HttpResponse('Ola')
+class EmployeeList(ListView):
+    model = Employee
+
+    def get_queryset(self):
+        company = self.request.user.employee.company
+        return Employee.objects.filter(company=company)
+
+
+class EmployeeEdit(UpdateView):
+    model = Employee
+    fields = ['name', 'departments']
+
+
+class EmployeeDelete(DeleteView):
+    model = Employee
+    success_url = reverse_lazy('list_employees')
+
+
+class EmployeeCreate(CreateView):
+    model = Employee
+    fields = ['name', 'departments']
+
+    def form_valid(self, form):
+        employee = form.save(commit=False)
+        username = employee.name
+        employee.company = self.request.user.employee.company
+        employee.user = User.objects.create(username=username)
+        employee.save()
+        return super(EmployeeCreate, self).form_valid(form)
