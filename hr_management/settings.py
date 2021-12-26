585de6140d19a7fa863634fe4d7c2b0fa58bb44d
Joao Gabriel <joaogabriel1598@gmail.com>
2021-12-26 11:50:07 -0300

    Update settings.py

--- hr_management/settings.py
+++ hr_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+3%tmuniq25+rfkk*bzjw1f51!hc*olrg0_y#ktb!lj@kgd2_x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.31.46.223']
 
 
 # Application definition
